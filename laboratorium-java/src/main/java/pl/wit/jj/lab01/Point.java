e66ef0252ebbce09f2efa1daf9f3e896303740d3
Mati <mateusz.wisniewski.pl@gmail.com>
2025-03-16 21:05:28 +0100

    comment Point class

--- laboratorium-java/src/main/java/pl/wit/jj/lab01/Point.java
+++ laboratorium-java/src/main/java/pl/wit/jj/lab01/Point.java
@@ -1,35 +1,84 @@
 package pl.wit.jj.lab01;
-
+/**
+* @author Mateusz Wiśniewski
+* 
+*  Klasa reprezentująca punkt w przestrzeni dwuwymiarowej.
+* Umożliwia przesuwanie punktu oraz wykonywanie operacji na współrzędnych.
+*
+*
+*/
 public class Point {
 
 	private float x ;
 	private float y ;
 
+	/**
+     * Konstruktor klasy Point inicjalizujący współrzędne punktu.
+     * 
+     * @param x wspsółrzędna X
+     * @param y współrzędna Y
+     */
+	
 	public Point(float x, float y) {
 		this.x = x;
 		this.y = y;
 
 	}
+  /**
+     * Przesuwa punkt wzdłuż osi X o podaną wartość.
+     * 
+     * @param shift wartość przesunięcia
+     */
 	public void moveX(float shift)
 	{
 		this.x +=shift;
 	}
+  /**
+     * Przesuwa punkt wzdłuż osi Y o podaną wartość.
+     * 
+     * @param shift wartość przesunięcia
+     */
 	public void moveY(float shift)
 	{
 		this.y +=shift;
 	}
+	 /**
+     * Zwraca współrzędną X punktu.
+     * 
+     * @return współrzędna X
+     */
 	public float getX() {
 		return x;
 	}
+	 /**
+     * Ustawia współrzędną X punktu.
+     * 
+     * @param x nowa wartość współrzędnej X
+     */
 	public void setX(float x) {
 		this.x = x;
 	}
+	  /**
+     * Zwraca współrzędną Y punktu.
+     * 
+     * @return współrzędna Y
+     */
 	public float getY() {
 		return y;
 	}
+	   /**
+     * Ustawia współrzędną Y punktu.
+     * 
+     * @param y nowa wartość współrzędnej Y
+     */
 	public void setY(float y) {
 		this.y = y;
 	}
+	 /**
+     * Dodaje współrzędne innego punktu do bieżącego punktu.
+     * 
+     * @param p2 punkt, którego współrzędne mają zostać dodane
+     */
 	public void add(Point p2) 
 	{
 		if (p2 == null)
@@ -41,6 +90,11 @@ public class Point {
 		moveY(p2.y);
 		
 	}
+	  /**
+     * Odejmuje współrzędne innego punktu od bieżącego punktu.
+     * 
+     * @param p2 punkt, którego współrzędne mają zostać odjęte
+     */
 	public void sub(Point p2) 
 	{
 		if (p2 == null)
@@ -52,6 +106,12 @@ public class Point {
 		moveY(-p2.y);
 		
 	}
+	  /**
+     * Tworzy nowy punkt będący sumą bieżącego punktu i podanego punktu.
+     * 
+     * @param p2 punkt do dodania
+     * @return nowy punkt reprezentujący sumę współrzędnych
+     */
 	public Point addAndCreate(Point p2) 
 	{
 		if (p2 == null)
--- laboratorium-java/src/test/java/pl/wit/jj/lab01/PointTest.java
+++ laboratorium-java/src/test/java/pl/wit/jj/lab01/PointTest.java
@@ -9,14 +9,18 @@ class PointTest {
 	@Test
 	void moveXTest() {
 		Point pt = new Point(5.31f,8.72f);
-		float  k = -3.5445674867f;
+		float  k = -0.31f;
 		pt.moveX(k);
-		assertEquals(2f,pt.getX());
+		assertEquals(5f,pt.getX());
 	}
 	void moveYTest() {
 		Point pt = new Point(5.31f,8.72f);
 		float  k = -3.31f;
 		pt.moveY(k);
 		assertEquals(5.41f,pt.getY());
+	}
+	void addTest() {
+		
+		
 	}
 }
