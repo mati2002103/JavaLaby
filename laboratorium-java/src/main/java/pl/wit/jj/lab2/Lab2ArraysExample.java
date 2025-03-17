c2fa1b4bab76e7392b52aad0dde9b4bbad10c708
mati2002103 <mateusz.wisniewski.pl@gmail.com>
2025-03-17 07:39:49 +0100

    123

---
+++ laboratorium-java/src/main/java/pl/wit/jj/lab2/Lab2ArraysExample.java
@@ -0,0 +1,17 @@
+package pl.wit.jj.lab2;
+
+public class Lab2ArraysExample {
+
+	/*
+	 * 
+	 * @author Mateusz Wiśniewski
+	 * 
+	 * Klasa z przykladami metod operującymi na tablicach
+	 * 
+	 * 
+	 */
+	private int intNumbers[]=null;
+	private String stringElements[]=null;
+	private boolean boolElements[]=null;
+	
+}
--- laboratorium-java/src/test/java/pl/wit/jj/lab01/PointTest.java
+++ laboratorium-java/src/test/java/pl/wit/jj/lab01/PointTest.java
@@ -13,14 +13,67 @@ class PointTest {
 		pt.moveX(k);
 		assertEquals(5f,pt.getX());
 	}
+	@Test
 	void moveYTest() {
-		Point pt = new Point(5.31f,8.72f);
-		float  k = -3.31f;
+		Point pt = new Point(5.31f,8.70f);
+		float  k = -3.30f;
 		pt.moveY(k);
-		assertEquals(5.41f,pt.getY());
+		assertEquals(5.3999996f,pt.getY());
 	}
+	@Test
 	void addTest() {
 		
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = new Point(4.69f,1.28f);
+		pt1.add(pt2);
+		assertEquals(10.f,pt1.getX());
+		assertEquals(10.f,pt1.getY());
 		
 	}
+	@Test
+	void NullAddTest() 
+	{
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = null;
+		pt1.add(pt2);
+		assertEquals(5.31f,pt1.getX());
+		assertEquals(8.72f,pt1.getY());
+		
+	}
+	@Test 
+	void subTest(){
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = new Point(4.69f,1.28f);
+		pt1.sub(pt2);
+		assertEquals(0.6199999f,pt1.getX());
+		assertEquals(7.4400005f,pt1.getY());
+	}
+	
+	@Test
+	void NullsubTest(){
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = null;
+		pt1.sub(pt2);
+		assertEquals(5.31f,pt1.getX());
+		assertEquals(8.72f,pt1.getY());
+	}
+	@Test
+	void addAndCreateTest(){
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = new Point(4.69f,1.28f);
+		Point pt3 =  pt1.addAndCreate(pt2);
+		assertEquals(pt3.getX(),pt1.getX()+pt2.getX());
+		assertEquals(pt3.getY(),pt1.getY()+pt2.getY());
+	}
+	@Test
+	void NullAddAndCreateTest(){
+		Point pt1 = new Point(5.31f,8.72f);
+		Point pt2 = null;
+		
+		Point pt3 =  pt1.addAndCreate(pt2);
+		assertEquals(pt3,null);
+	
+	}
+	
+	
 }
